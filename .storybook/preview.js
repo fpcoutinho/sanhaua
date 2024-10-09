365d27bd556721a3f52ca834863dae348262fd46
fpcoutinho <fpcoutinho5@gmail.com>
2024-10-09 14:43:30 -0300

    add light/dark mode

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -5,9 +5,47 @@ const preview = {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i
+      },
+      sort: 'requiredFirst'
+    },
+    html: {
+      prettier: {
+        tabWidth: 4,
+        useTabs: true,
+        htmlWhitespaceSensitivity: 'strict'
+      }
+    }
+  },
+  globalTypes: {
+    templateMode: {
+      description: 'Template mode',
+      defaultValue: 'light',
+      toolbar: {
+        title: 'Template mode',
+        icon: 'mirror',
+        items: [
+          {
+            value: 'light',
+            title: 'Light'
+          },
+          {
+            value: 'dark',
+            title: 'Dark'
+          }
+        ],
+        dynamicTitle: true
       }
     }
   }
 }
+
+const WithThemeProvider = (Story, context) => {
+  const canvasElement = context.canvasElement
+  canvasElement.classList.remove('light', 'dark')
+  canvasElement.classList.add('sanhaua', context.globals.templateMode)
+  return Story()
+}
+
+export const decorators = [WithThemeProvider]
 
 export default preview
