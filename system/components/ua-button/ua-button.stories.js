84b3c24a9c41d9ba0e437db0b246815acfa87ec2
fpcoutinho <fpcoutinho5@gmail.com>
2024-10-09 14:46:00 -0300

    fix subtitle

--- system/components/ua-button/ua-button.stories.js
+++ system/components/ua-button/ua-button.stories.js
@@ -8,7 +8,8 @@ export default {
   tags: ['autodocs'],
   parameters: {
     docs: {
-      subtitle: 'Provides an action point for the user to carry out specific actions.'
+      subtitle:
+        'An interactive element activated by a user with a mouse, keyboard, finger, voice command, or other assistive technology. Once activated, it then performs an action, such as submitting a form or opening a dialog.'
     }
   },
   argTypes: {
