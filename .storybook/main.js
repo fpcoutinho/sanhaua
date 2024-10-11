430be67b22c886e047ccb465ec03f0fb8cb76057
fpcoutinho <fpcoutinho5@gmail.com>
2024-10-11 05:07:20 -0300

    update deps

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,10 +2,8 @@
 const config = {
   stories: ['../storybook/**/*.mdx', '../system/components/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    '@storybook/addon-onboarding',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@chromatic-com/storybook',
     '@storybook/addon-interactions',
     '@storybook/addon-a11y'
   ],
