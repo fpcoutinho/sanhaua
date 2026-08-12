82fecafd0c21f9938403e36a50a3627a3f8afa22
Filipe Coutinho <fpcoutinho5@gmail.com>
2026-08-11 21:55:33 -0300

    fix: react ds

--- .storybook/react/main.js
+++ .storybook/react/main.js
@@ -67,10 +67,41 @@ const config = {
 
   previewHead: head => `
     ${head}
+    <style>
+      @import "https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:wght,GRAD@100..700,-25";
+      @import "https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap";
+
+      body * {
+        font-family: "Lato", sans-serif !important;
+      }
+
+      .material-symbols-rounded {
+        font-family: 'Material Symbols Rounded' !important;
+        font-weight: normal;
+        font-style: normal;
+        font-size: 24px;
+        line-height: 1;
+        letter-spacing: normal;
+        text-transform: none;
+        display: inline-block;
+        white-space: nowrap;
+        word-wrap: normal;
+        direction: ltr;
+        -webkit-font-smoothing: antialiased;
+      }
+    </style>
   `,
 
   managerHead: head => `
     ${head}
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <style>
+      @import "https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap";
+
+      body * {
+        font-family: "Lato", sans-serif !important;
+      }
+    </style>
   `
 }
 
